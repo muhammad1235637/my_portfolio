75709540010ee0d05616ba098c967fc68d842e9e
muhammad1235637 <muhammad1235637@gmail.com>
2026-03-20 16:00:15 +0300

    update

---
+++ script/script.js
@@ -0,0 +1,23 @@
+async function loadProjects() {
+    const response = await fetch('/data/projects.json');
+    const projects = await response.json();
+    const container = document.querySelector('.projects-feed');
+    
+    container.innerHTML = projects.map(project => `
+        <div class="project-entry">
+            <div class="project-meta">
+                <span class="id">ID: ${project.id}</span>
+                <span class="tag">${project.category}</span>
+            </div>
+            <h2>${project.title}</h2>
+            <p>${project.desc}</p>
+            <div class="tech-used">
+                ${project.tech.map(t => `<span>${t}</span>`).join('')}
+            </div>
+            <div class="project-links">
+                <a href="${project.link}">[VIEW_SOURCE]</a>
+            </div>
+        </div>
+    `).join('');
+}
+loadProjects();
